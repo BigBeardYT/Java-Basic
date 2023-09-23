69ec265443038fbbd6198b9d8abdbd4d42cc0efc
BigBeardYT <627944524@qq.com>
2023-09-23 11:13:58 +0800

    "some add"

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int n = 9;
+        for(int i = 1;i < n;i++){
+            for(int j = 1;j < n;j++){
+                System.out.print("i * j = " + i * j);
+                if(j == i){
+                    System.out.println();
+                }
+            }
+        }
     }
 }
---
+++ src/Var_Use.java
@@ -0,0 +1,12 @@
+public class Var_Use {
+    public static void main(String[] args) {
+        // jvm会自动根据var后面new的内容生成对应的变量
+        // StringBuffer sb = new StringBuffer();
+        var sb = new StringBuffer();
+        for(int i = 0;i < 10;i++){
+            sb.append(String.valueOf(i));
+        }
+        System.out.println(sb.toString());
+
+    }
+}
