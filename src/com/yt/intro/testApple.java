5c440538ee436c761d0b92d352801420c05d291a
BigBeardYT <627944524@qq.com>
2023-10-15 16:19:37 +0800

    "some add or revice"

--- src/com/yt/intro/testApple.java
+++ src/com/yt/intro/testApple.java
@@ -7,6 +7,18 @@ public class testApple {
          */
         // Fruit f1 = new Fruit(); // 'Fruit' is abstract; cannot be instantiated
 
+        /**
+         * 抽象类不可以被实例化，但是，可以使用main方法来调用，也可以写入初始化块体现，也可以在main方法中写对象。
+         */
+        Fruit f1 = new Fruit() {
+            @Override
+            public void run() {
+                System.out.println("Fruit run()");
+            }
+        };
+        f1.run();
+
+
         Apple a1 = new Apple();
         a1.run();
 
