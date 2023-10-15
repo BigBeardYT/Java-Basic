a3db8f595271fdd6f16665f5e6a514bcf65a394b
BigBeardYT <627944524@qq.com>
2023-10-15 17:22:27 +0800

    "some add or revice"

--- src/com/yt/intro/testAbsIncome.java
+++ src/com/yt/intro/testAbsIncome.java
@@ -15,7 +15,12 @@ public class testAbsIncome {
         System.out.println("—————————————方法一———————————————\n各种收入的分别收税为:\nIncome Tax: " +
                             tax1 + "\nSalary Tax: " + tax2 +"\nSpeical Tax: " + tax3 + "\n总收入为: " + total_tax);
         /**
-         * 方法二，直接将父类引用指向子类的实例
+         * 方法二，直接将父类作为变量类型数组，引用指向子类的实例
+         * 因为这里实际上并没有对AbsIncome类进行实例化，AbsIncome[]数组中实际上实例化的是 new AIncome(),new ASalary()和ASpecialAllowance
+         * Income类只是变量类型而非实例类型。
+         *
+         * 就好像上面的教程当中有Person类为抽象类，Student类和Teacher类则为具体的子类，Person p = new Student()这样写是没有问题的，
+         * 因为本质上实例化的是Student而不是Person类，只要不是AbsIncome abs = new AbsIncome();就行
          */
         AbsIncome [] absIncomes = new AbsIncome[] {
                 new AIncome(3000),
