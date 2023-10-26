56495f1e86657fbcd57f8a73f1731ea98fdf015f
BigBeardYT <627944524@qq.com>
2023-10-26 16:00:02 +0800

    "some add or revice"

--- src/com/yt/genericity/testSuperT.java
+++ src/com/yt/genericity/testSuperT.java
@@ -8,6 +8,9 @@ public class testSuperT {
         p.setFirst(123);
         Object x2 = p.getFirst();
         System.out.println(x2);
-
+        Pair<String> p2 = new Pair<>("sdd", "qqq");
+        // 可以安全的向上转型
+        Pair<?> p3 = p;
+        System.out.println(p2.getFirst() + ", " + p2.getLast());
     }
 }
