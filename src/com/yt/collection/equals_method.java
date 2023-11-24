2b4128be0727128e7e7eac42be73f8f64bb29d1b
BigBeardYT <627944524@qq.com>
2023-11-24 17:21:40 +0800

    "some add or revice"

--- src/com/yt/collection/equals_method.java
+++ src/com/yt/collection/equals_method.java
@@ -10,20 +10,24 @@ public class equals_method {
     public static void main(String[] args) {
         List<String> list = new ArrayList<>();
         list = List.of("A", "B", "C");
-        System.out.println(list.toString());
-        System.out.println(list.contains("C"));
-        System.out.println(list.contains(new String("C"))); // 因为List 的内部是通过equals来判断的，而不是==
-        System.out.println(list.indexOf(new String("C")));
-        System.out.println(list.get(2) == new String("C"));
-        System.out.println(list.get(2).equals(new String("C")));
+        System.out.println(list.toString());        // [A, B, C]
+        System.out.println(list.contains("C"));     // true
+        System.out.println(list.contains(new String("C"))); // true -> 因为List 的内部是通过equals来判断的，而不是==
+        System.out.println(list.indexOf(new String("C")));  // 2
+        System.out.println(list.get(2) == new String("C")); // false
+        System.out.println(list.get(2).equals(new String("C")));    // true
 
         // person类实验
         List<Person> personList = List.of(
-                new Person("Xiao Ming"),
-                new Person("Mary"),
-                new Person("Bob"));
+                new Person("Xiao Ming", 12),
+                new Person("Mary", 13),
+                new Person("Bob", 14));
         // 没有重写equals方法的结果：
-        System.out.println(personList.contains("Mary"));    // --> false
+        System.out.println(personList.contains(new Person("Mary", 13)));    // --> true
+
+        // 使用indexOf方法返回索引
+        System.out.println(personList.indexOf(new Person("Mary", 13)));
+
     }
 
 
@@ -32,9 +36,12 @@ public class equals_method {
 class Person {
     String name;
     int age;
-    public Person(String name) {
-        this.name = name;
+    public Person(String name, int age) {
+        this.name = name;this.age = age;
     }
+
+
+
 
 //    public boolean equals(Object o) {
 //        if(o instanceof Person p) {
@@ -42,21 +49,32 @@ class Person {
 //        }
 //        return false;
 //    }
+//    public int indexOf(Object o) {
+//        if(o instanceof Person p) {
+//            if(Objects.equals(this.name, p.name) && this.age == p.age) {
+//                return 1;
+//            }
+//        }
+//        return -1;
+//    }
 
-    public boolean equals1(Object o) {
-        if(o instanceof Person p) {
-            boolean nameEquals = false;
-            // 判断是否为空
-            if(this.name == null && p.name == null) {
-                return true;
-            }
-            if(this.name != null) {
-                nameEquals = this.name.equals(p.name);
-            }
-            return nameEquals && this.age == p.age;
-        }
-        return false;
-    }
+
+//    public boolean equals1(Object o) {
+//        if(o instanceof Person p) {
+//            boolean nameEquals = false;
+//            // 判断是否为空
+//            if(this.name == null && p.name == null) {
+//                return true;
+//            }
+//            if(this.name != null) {
+//                nameEquals = this.name.equals(p.name);
+//            }
+//            return nameEquals && this.age == p.age;
+//        }
+//        return false;
+//    }
+
+    @Override
     public boolean equals(Object o) {
         if(o instanceof Person p) {
             return Objects.equals(this.name, p.name) && this.age == p.age;
---
+++ src/com/yt/collection/test_Student.java
@@ -0,0 +1,61 @@
+package com.yt.collection;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.List;
+
+public class test_Student {
+    public static void main(String[] args) {
+//        Student s = new Student("Bob", 84);
+        List<Student> studentList = List.of(
+                new Student("Bob", 84),
+                new Student("Mary", 87),
+                new Student("Joy", 98)
+        );
+        Students students = new Students(studentList);
+        System.out.println(students.getScore("Bob"));
+    }
+}
+
+class Students {
+    List<Student> list;
+    Map<String, Integer> cache;
+
+    Students(List<Student> list) {
+        this.list = list;
+        cache = new HashMap<>();
+    }
+
+    /**
+     * 根据name查找score，找到返回score，未找到返回-1
+     */
+    int getScore(String name) {
+        Integer score = cache.get(name);
+        if(score == null) {
+            // 分数为空，将其put进去
+            int sr = findInList(name);
+            cache.put(name, sr);
+        }
+        return cache.get(name);
+    }
+
+    Integer findInList(String name) {
+        for(Student ss : this.list) {
+            if(ss.name.equals(name)) {
+                return ss.score;
+            }
+        }
+        return null;
+    }
+
+}
+
+class Student {
+    public String name;
+    public int score;
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+}
